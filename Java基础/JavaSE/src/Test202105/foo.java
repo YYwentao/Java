69027619b5a027c71042e9e7f87ca6b725aac210
yywentao <1938145653@qq.com>
2021-06-01 23:28:34 +0800

    每日一题

--- Java基础/JavaSE/src/Test202105/foo.java
+++ Java基础/JavaSE/src/Test202105/foo.java
@@ -9,5 +9,33 @@ package Test202105;
  * @since JDK 1.8
  */
 public class foo {
+    public static int StrToInt(String str) {
+        if(str == null || str.length() == 0) {
+            return 0;
+        }
+        char[] array = str.toCharArray();
+        int flg = 1;
+        if(array[0] == '+') {
+            array[0] = '0';
+            flg = 1;
+        } else if (array[0] == '-') {
+            array[0] = '0';
+            flg = -1;
+        } else {
+            ;
+        }
+        int count = 0;
+        for(int i = 0; i < array.length; i++) {
+            if(array[i] < '0' || array[i] > '9' ) {
+                return 0;
+            }
+            count = count * 10 + (array[i] -'0');
+        }
+        return count*flg;
+    }
+
+    public static void main(String[] args) {
+        StrToInt("+2147483647");
+    }
 
 }
