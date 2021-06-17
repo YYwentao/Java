496788ce87437a1dafffae9dbba1582d059bb8d8
yywentao <1938145653@qq.com>
2021-06-17 21:19:08 +0800

    字节流

--- Java基础/JavaSE/src/Test202106/Test20210616.java
+++ Java基础/JavaSE/src/Test202106/Test20210616.java
@@ -1,6 +1,6 @@
 package Test202106;
 
-import java.io.File;
+import java.io.*;
 
 /**
  * ClassName: Test20210616
@@ -39,6 +39,44 @@ public class Test20210616 {
     }
 
     /**
-     *
+     *  字节流
+     *  带缓冲区的读和写，速度比不带缓冲区的读和写快，要引入 BufferedInputStream 和 BufferedOutputStream
      */
+    public static void main(String[] args) throws IOException {
+        //版本1 代码丑且发生异常不会close
+        copyFile();
+        //版本2 适用
+        copyFile1();
+    }
+
+
+    private static void copyFile() throws IOException {
+        //创建实例 BufferedInputStream 和 BufferedOutputStream
+        FileInputStream fileInputStream = new FileInputStream("E:/images/image.jpg");
+        FileOutputStream fileOutputStream = new FileOutputStream("E:/images/image1.jpg");
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+        byte[] buffer = new byte[1024];
+        int length = -1;
+        while((length = bufferedInputStream.read(buffer)) != -1) {
+            bufferedOutputStream.write(buffer,0,length);
+        }
+        //此时涉及到四个流对象
+        // 关闭后面两个就可以了
+        bufferedInputStream.close();;
+        bufferedOutputStream.close();
+    }
+
+    private static void copyFile1() {
+        try {BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("E:/images/image.jpg"));
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("E:/images/image2.jpg"));
+             byte[] buffer = new byte[1024];
+             int length = -1;
+             while((length = bufferedInputStream.read(buffer)) != -1) {
+                 bufferedOutputStream.write(buffer,0,length);
+             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
