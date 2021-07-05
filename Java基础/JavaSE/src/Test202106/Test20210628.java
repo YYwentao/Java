abb587f8689e16467ef56a7f1ca593e831aeeb4f
yywentao <1938145653@qq.com>
2021-07-05 22:33:16 +0800

    练习

---
+++ Java基础/JavaSE/src/Test202106/Test20210628.java
@@ -0,0 +1,16 @@
+package Test202106;
+
+/**
+ * ClassName: Test20210628
+ * Description:
+ * date: 2021/6/28 15:47
+ *
+ * @author wt
+ * @since JDK 1.8
+ */
+
+public class Test20210628 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Java基础/JavaSE/src/Test20210705.java
@@ -0,0 +1,13 @@
+/**
+ * ClassName: Test20210705
+ * Description:
+ * date: 2021/7/5 22:32
+ *
+ * @author wt
+ * @since JDK 1.8
+ */
+public class Test20210705 {
+    public static void main(String[] args) {
+        
+    }
+}
