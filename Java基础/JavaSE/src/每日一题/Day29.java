4a32a868fed43129855ceabd9d0af5d967bcb05a
yywentao <1938145653@qq.com>
2021-07-24 23:34:48 +0800

    每日一题

---
+++ Java基础/JavaSE/src/每日一题/Day29.java
@@ -0,0 +1,12 @@
+package 每日一题;
+
+/**
+ * ClassName: Day29
+ * Description:
+ * date: 2021/7/24 21:01
+ *
+ * @author wt
+ * @since JDK 1.8
+ */
+public class Day29 {
+}
---
+++ Java基础/JavaSE/src/每日一题/Day30.java
@@ -0,0 +1,28 @@
+package 每日一题;
+
+import java.util.Scanner;
+
+/**
+ * ClassName: Day30
+ * Description:
+ * date: 2021/7/24 21:01
+ *
+ * @author wt
+ * @since JDK 1.8
+ */
+public class Day30 {
+    //[编程题]有假币
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while(scan.hasNext()) {
+            int n = scan.nextInt();
+            int count = 0;
+            if(n == 0) return;
+            while(n >=2) {
+                count++;
+                n = (int)Math.ceil(((double)n)/3);
+            }
+            System.out.println(count);
+        }
+    }
+}
