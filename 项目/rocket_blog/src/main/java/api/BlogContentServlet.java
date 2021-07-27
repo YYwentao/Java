a3ebf570ad74df52ff92ab7e28a347599d0aba6e
yywentao <1938145653@qq.com>
2021-07-27 22:52:32 +0800

    博客项目2.0版本

---
+++ 项目/rocket_blog/src/main/java/api/BlogContentServlet.java
@@ -0,0 +1,55 @@
+package api;
+
+import dao.Blog;
+import dao.BlogDao;
+import dao.User;
+import dao.UserDao;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Blob;
+
+/**
+ * ClassName: BlogContentServlet
+ * Description:
+ * date: 2021/7/23 16:17
+ *
+ * @author wt
+ * @since JDK 1.8
+ */
+@WebServlet("/blogContent")
+public class BlogContentServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        int blogId = Integer.parseInt(req.getParameter("blogId"));
+        // 1.执行 dao 层数据库
+        BlogDao blogDao = new BlogDao();
+        Blog blog = blogDao.selectById(blogId);
+        if (blog == null) {
+//            resp.sendError(404," blogId 的文章不存在！");
+            resp.getWriter().write("<h3>blogId 的文章不存在！</h3>");
+            return;
+        }
+        // 2.根据 blog.userId 找到对应的 dao.User 对象
+        UserDao userDao = new UserDao();
+        User user = userDao.selectById(blog.getUserId());
+        // 3.构造博客详细页面
+        //  1).需要先搞一个 WebContext 对象(数据集合)
+        //  把想要替换的关键数据放到收集起来,放到 WebContext 中
+        WebContext webContext = new WebContext(req,resp,getServletContext());
+        webContext.setVariable("blog",blog);
+        webContext.setVariable("username",user.getUsername());
+        // 4.查询结果渲染到 html 模板中
+        TemplateEngine engine = (TemplateEngine) getServletContext().getAttribute("engine");
+        String html = engine.process("blogContent",webContext);
+        resp.getWriter().write(html);
+    }
+}
---
+++ 项目/rocket_blog/src/main/java/api/BlogInsertServlet.java
@@ -0,0 +1,53 @@
+package api;
+
+import dao.Blog;
+import dao.BlogDao;
+import dao.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * ClassName: BlogInsertServlet
+ * Description:
+ * date: 2021/7/26 21:00
+ *
+ * @author wt
+ * @since JDK 1.8
+ */
+@WebServlet("/blogInsert")
+public class BlogInsertServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        // 0.获取到当前登录的用户
+        User user = (User) req.getSession().getAttribute("user");
+        if (user == null) {
+//            resp.sendError(404,"当前未登录用户！不能发布博客！");
+            resp.getWriter().write("<h3>当前未登录用户！不能发布博客！</h3>");
+            return;
+        }
+        // 1.获取请求中的内容
+        String title = req.getParameter("title");
+        String content = req.getParameter("content");
+        if (title == null || "".equals(title) || content == null || "".equals(content)) {
+//            resp.sendError(404,"博客标题或内容不能为空");
+            resp.getWriter().write("<h3>博客标题或内容不能为空</h3>");
+            return;
+        }
+        // 2.构造 blog 对象 ，执行 blogDao 插入数据库
+        Blog newBlog = new Blog();
+        newBlog.setUserId(user.getUserId());
+        newBlog.setTitle(title);
+        newBlog.setContent(content);
+        BlogDao blogDao = new BlogDao();
+        blogDao.insert(newBlog);
+        // 3.返回博客列表页面
+        resp.sendRedirect("blog_list");
+    }
+}
