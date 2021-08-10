8e282ea123006c548c6c31b67fa8d1d90b6b11a0
yywentao <1938145653@qq.com>
2021-08-10 22:32:30 +0800

    排序

--- Java基础/JavaSE/src/第一轮复习开始/Sort.java
+++ Java基础/JavaSE/src/第一轮复习开始/Sort.java
@@ -2,6 +2,8 @@ package 第一轮复习开始;
 
 
 import java.util.Arrays;
+import java.util.Random;
+import java.util.Stack;
 
 
 /**
@@ -25,11 +27,141 @@ public class Sort {
 //        System.out.println(Arrays.toString(arr));
 
         //堆排序
-        heapSort(arr);
-        System.out.println(Arrays.toString(arr));
+//        heapSort(arr);
+//        System.out.println(Arrays.toString(arr));
 
 //        bubbleSort(arr);
 //        System.out.println(Arrays.toString(arr));
+
+        //快速排序
+//        Random random = new Random();
+//        int[] arr = new int[10_000];
+//        for (int i = 0; i < 10_000; i++) {
+//            arr[i] = random.nextInt(1000)+1;
+//        }
+//        System.out.println(Arrays.toString(arr));
+//        quickSort(arr);
+//        System.out.println(Arrays.toString(arr));
+        //非递归快排
+//        quickSort2(arr);
+//        System.out.println(Arrays.toString(arr));
+        //归并排序
+        System.out.println(Arrays.toString(arr));
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    //归并排序
+    private static void mergeSort(int[] arr) {
+        merge(arr,0,arr.length-1);
+    }
+
+
+    private static void merge(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int mid = (left+right) >>> 1;
+        merge(arr,left,mid);
+        merge(arr,mid+1,right);
+        //合并
+        mergeRet(arr,left,mid,right);
+    }
+
+    private static void mergeRet(int[] arr, int left, int mid, int right) {
+        int[] tmpArr = new int[right-left+1];
+        int k = 0;
+        int s1 = left;
+        int e1 = mid;
+        int s2 = mid+1;
+        int e2 = right;
+        while (s1 <= e1 && s2 <= e2) {
+            if (arr[s1] <= arr[s2]) {
+                tmpArr[k++] = arr[s1++];
+            } else {
+                tmpArr[k++] = arr[s2++];
+            }
+        }
+        while(s1 <= e1) {
+            tmpArr[k++] = arr[s1++];
+        }
+        while (s2 <= e2) {
+            tmpArr[k++] = arr[s2++];
+        }
+        for (int i = 0; i < tmpArr.length; i++) {
+            arr[left+i] = tmpArr[i];
+        }
+    }
+
+    ////非递归快排
+    private static void quickSort2(int[] arr) {
+        int right = arr.length - 1;
+        int left = 0;
+        int par = partion(arr,left,right); //找 基准
+        Stack<Integer> stack = new Stack<>();
+        if (par > left+1) {
+            stack.push(left);
+            stack.push(par-1);
+
+        }
+        if (par < right-1) {
+            stack.push(par+1);
+            stack.push(right);
+        }
+        while (!stack.empty()) {
+            right = stack.pop();
+            left = stack.pop();
+            par = partion(arr,left,right);
+            if (par > left+1) {
+                stack.push(left);
+                stack.push(par-1);
+
+            }
+            if (par < right-1) {
+                stack.push(par+1);
+                stack.push(right);
+            }
+        }
+    }
+
+    //递归快排
+    private static void quickSort(int[] arr) {
+        quick(arr,0,arr.length-1);
+    }
+
+    private static void quick(int[] arr, int left, int right) {
+
+        if (left >= right) {
+            return;
+        }
+        int par = partion(arr,left,right);  //找基准
+        quick(arr,left,par - 1);
+        quick(arr,par + 1,right);
+    }
+
+    private static int partion(int[] arr, int s, int e) {
+        int tmp = arr[s];
+        while (s < e) {
+            while (s < e && arr[e] >= tmp) {
+                e--;
+            }
+            if (s >= e) {
+                arr[s] = tmp;
+                break;
+            } else {
+                arr[s] = arr[e];
+            }
+
+            while (s < e && arr[s] <= tmp) {
+                s++;
+            }
+            if (s >= e) {
+                arr[s] = tmp;
+                break;
+            } else {
+                arr[e] = arr[s];
+            }
+        }
+        return s;
     }
 
     //冒泡排序
